fdf5f01f8b3eede9783a71978979ac70e3d0e43c
MatasCze <82163164+MatasCze@users.noreply.github.com>
2022-06-24 17:06:11 +0200

    added .gitignore, added functionality to run program by entering user input from command line, added exceptions

--- main.py
+++ main.py
@@ -4,22 +4,19 @@ import musicbrainzngs
 musicbrainzngs.set_useragent(
     "MusicFetcherByWord", "1.0", contact="matasczepla@gmail.com")
 
-def randomWordGenerator(word_count):
+def random_word_generator(word_count):
     #get response as a json object, extract the words and place them in a list
-    word_list = []
-    if (word_count < 5) or (word_count > 20):
-        print("Integer out of range")
-    else:
-        counter = 0
-        while(counter < word_count):
-            response = requests.get('https://random-words-api.vercel.app/word')
-            json_response = response.json()
-            word_from_json_response = json_response[0].get('word')
-            if word_from_json_response not in word_list:
-                word_list.append(word_from_json_response)
-                counter += 1
-        word_list.sort()
-        return word_list
+    word_list = []   
+    counter = 0
+    while(counter < word_count):
+        response = requests.get('https://random-words-api.vercel.app/word')
+        json_response = response.json()
+        word_from_json_response = json_response[0].get('word')
+        if word_from_json_response not in word_list:
+            word_list.append(word_from_json_response)
+            counter += 1
+    word_list.sort()
+    return word_list
 
 def fetch_and_print_recordings(word_list):
     for word in word_list:
@@ -32,9 +29,20 @@ def fetch_and_print_recordings(word_list):
         else:
             print('For keyword ' + word + ' - no recording found!' + '\n')
 
-try:
-    generated_words = randomWordGenerator(20)
-    print(generated_words) 
-    fetch_and_print_recordings(generated_words)
-except TypeError as e:
-    print("Type error message: {}".format(e))
+if __name__ == '__main__':
+    import sys
+    user_input = sys.argv[1]
+    try:
+        user_input_word_count = int(user_input)
+        if (user_input_word_count < 5) or (user_input_word_count > 20):
+            print("Custom error message: integer out of range, must be between 5 and 20")
+        else:
+            try:       
+                generated_words = random_word_generator(user_input_word_count)
+                print(generated_words) 
+                fetch_and_print_recordings(generated_words)
+            except TypeError as e:
+                print("Type error message: {}".format(e))
+    except ValueError as e:
+        print("Value error message: {}".format(e))
+    
