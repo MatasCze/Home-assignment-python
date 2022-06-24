d226ea69eea8fbbe7862e15ec9f898f42094051c
MatasCze <82163164+MatasCze@users.noreply.github.com>
2022-06-24 16:41:26 +0200

    Added function that presents for each word top one recording with it's title, artist and album names

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
+import musicbrainzngs
 
-
-
-word_list = []
+musicbrainzngs.set_useragent(
+    "MusicFetcherByWord", "1.0", contact="matasczepla@gmail.com")
 
 def randomWordGenerator(word_count):
     #get response as a json object, extract the words and place them in a list
+    word_list = []
     if (word_count < 5) or (word_count > 20):
         print("Integer out of range")
     else:
@@ -17,9 +18,23 @@ def randomWordGenerator(word_count):
             if word_from_json_response not in word_list:
                 word_list.append(word_from_json_response)
                 counter += 1
+        word_list.sort()
         return word_list
 
+def fetch_and_print_recordings(word_list):
+    for word in word_list:
+        result = musicbrainzngs.search_recordings("'" + word + "'")
+        if (len(result.get('recording-list')) != 0):
+            title = result.get('recording-list')[0].get('title')
+            album = result.get('recording-list')[0].get('release-list')[0].get('release-group').get('title')
+            artist = result.get('recording-list')[0].get('artist-credit')[0].get('name')
+            print("For keyword {} found:\nArtist: {}\n Album: {}\n Title: {}\n".format(word, artist, album, title))
+        else:
+            print('For keyword ' + word + ' - no recording found!' + '\n')
+
 try:
-    print(randomWordGenerator(5))
+    generated_words = randomWordGenerator(20)
+    print(generated_words) 
+    fetch_and_print_recordings(generated_words)
 except TypeError as e:
     print("Type error message: {}".format(e))
